8611255661b8dd4d2eb8d57107d7dfdbe0558dfa
Dyson Returns <4380544+dysonreturns@users.noreply.github.com>
2023-03-19 17:26:19 -0700

    Adds Unit.tech_alias and Unit.unit_alias to data.json and data_readable.json

--- generate/collect.py
+++ generate/collect.py
@@ -261,6 +261,10 @@ class MyBot(BotAI):
             if a._proto.tech_alias:
                 alias = min(a._proto.tech_alias)
 
+        tech_aliases = [
+            UnitTypeId(tech_alias).value for tech_alias in a._proto.tech_alias if tech_alias in self._game_data.units
+        ]
+
         is_worker = a.id in {UnitTypeId.PROBE, UnitTypeId.DRONE, UnitTypeId.SCV}
         is_townhall = a.id in {
             UnitTypeId.NEXUS,
@@ -356,6 +360,8 @@ class MyBot(BotAI):
             "minerals": a._proto.mineral_cost,
             "gas": a._proto.vespene_cost,
             "time": a._proto.build_time,
+            "tech_alias": tech_aliases,
+            "unit_alias": a._proto.unit_alias,
         }
 
     async def on_start(self) -> None:
